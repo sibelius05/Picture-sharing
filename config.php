bda7578a0e0852bf22fdb10e649077b659c22667
sibelius05 <129514002+sibelius05@users.noreply.github.com>
2023-04-17 19:42:18 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
 // für mysqli-Anschluss
-const DB_SERVER = 'rdbms.strato.de';
-const DB_USER = 'dbu640831';
-const DB_PASSWD = 'buIu!fH?9((mW|Ry^R@C';
-const DB_NAME = 'dbs8313657';
+const DB_SERVER = '';
+const DB_USER = '';
+const DB_PASSWD = '';
+const DB_NAME = '';
 
 const NEW_DATABASE = false;
 
-const MAIL_USER = 'ulrich.grefe@halbanalog.info';
-const MAIL_PASSWD = '^__OSx+$LgeM/.YTKGPe';
-const SMTP_HOST = 'ssl://smtp.strato.de';
+const MAIL_USER = '';
+const MAIL_PASSWD = '';
+const SMTP_HOST = '';
 const SMTP_PORT = '465';
 
 const SPECIAL_CHARS = '!#$%&()*+,-.:;/<>=?@^_{|}~';
@@ -25,7 +25,7 @@ const COMPRESSION_LEVEL_UPLOAD = 80;
 const COMPRESSION_LEVEL_DOWNLOAD = 90;
 const COMPRESSION_TYPE = 'webp';
 
-const EMAIL_ADMIN = 'ulrich.grefe@web.de';
-const FIRSTNAME_ADMIN = 'Ulrich';
-const LASTNAME_ADMIN = 'Grefe';
+const EMAIL_ADMIN = '';
+const FIRSTNAME_ADMIN = '';
+const LASTNAME_ADMIN = '';
 
