f1414e051400db2b6eea7de1c29c570768dab250
sibelius05 <129514002+sibelius05@users.noreply.github.com>
2023-04-17 19:38:44 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+// für mysqli-Anschluss
+const DB_SERVER = 'rdbms.strato.de';
+const DB_USER = 'dbu640831';
+const DB_PASSWD = 'buIu!fH?9((mW|Ry^R@C';
+const DB_NAME = 'dbs8313657';
+
+const NEW_DATABASE = false;
+
+const MAIL_USER = 'ulrich.grefe@halbanalog.info';
+const MAIL_PASSWD = '^__OSx+$LgeM/.YTKGPe';
+const SMTP_HOST = 'ssl://smtp.strato.de';
+const SMTP_PORT = '465';
+
+const SPECIAL_CHARS = '!#$%&()*+,-.:;/<>=?@^_{|}~';
+const DAYS_REGISTRATION = 5;
+
+const DURATION_SESSION = 3600;
+
+const SIZE_VISITOR = 600;
+const SIZE_UNACTIVE_USER = 800;
+const RESOLUTION_UPLOADS = 5500000; //1298000;
+const COMPRESSION_LEVEL_VIEW = 70;
+const COMPRESSION_LEVEL_UPLOAD = 80;
+const COMPRESSION_LEVEL_DOWNLOAD = 90;
+const COMPRESSION_TYPE = 'webp';
+
+const EMAIL_ADMIN = 'ulrich.grefe@web.de';
+const FIRSTNAME_ADMIN = 'Ulrich';
+const LASTNAME_ADMIN = 'Grefe';
+
